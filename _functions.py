2957c65b495adb813f73be805ea81ea08d32440e
JTEELE <j.teele@outlook.com>
2022-02-20 01:22:25 -0600

    updated functionality and simplified main.py

--- _functions.py
+++ _functions.py
@@ -1,9 +1,11 @@
 import os
 import re
+from pathlib import Path
+salaries_benefits_geo = 42
+current_month = '2021-12-31'
 import pandas as pd
 from openpyxl import Workbook, load_workbook
-from pathlib import Path
-
+from _functions import *
 
 def rename():
     owd = os.getcwd()
@@ -12,15 +14,20 @@ def rename():
         os.chdir(r"execute")
         for pname in plist :
             os.rename(pname, re.sub('[^A-Za-z.]', '' , (pname)))
+    except:
+        pass
     finally:
         os.chdir('../')
 rename()
 
-
-def income_statement(trial_balance):
-    current_month = '2021-12-31'
+def income_statement():
+    try:
+        rename()
+    except:
+        pass
     wb = load_workbook('execute/GAAPIS.xlsx')
     ws = wb.active
+
     dates = pd.Series(['2021-01-31', '2021-02-28', '2021-03-31','2021-04-30','2021-05-31','2021-06-30','2021-07-31','2021-08-31','2021-09-30','2021-10-31','2021-11-30','2021-12-31'])
     idx = pd.Series(['D','E','F','G','H','I','J','K','L','M','N','O'])
     idx.index = dates 
@@ -41,13 +48,15 @@ def income_statement(trial_balance):
     oibit = '32'
     pfit = '34'
     net_income_id = '36'
-    salaries_benefits_geo = 42
-    data = pd.read_csv('support/tb-modified.csv', "_")
+
+
+    data = pd.read_csv('support/12-31-2021.csv', "_")
     data.columns = ["Company","GLAcct", "CurrentMo", "PriorMo", "Abs_Diff"]
     df = data[data.GLAcct.notnull() & data.CurrentMo.notnull() ]
     df['GLAcct'] = df['GLAcct'].str.replace(" ","_")
     df = df.set_index('GLAcct')
 
+    #Income
     interest_income = round(float(df['Abs_Diff'].loc['Total_Interest_Income'])/1000)*-1
     interest_expense = round(float(df['Abs_Diff'].loc['Total_Interest_Expense'])/1000)
     niibpcl = round(interest_income-interest_expense)
@@ -78,17 +87,17 @@ def income_statement(trial_balance):
     ws[f'{idx[current_month]}{interest_exp}'].value = interest_expense
     ws[f'{idx[current_month]}{gnii}'].value = f'={idx[current_month]}{interest_inc}-{idx[current_month]}{interest_exp}'
     ws[f'{idx[current_month]}{pcl}'].value = provision_credit_losses
-    ws[f'{idx[current_month]}{nii}'].value = net_interest_income
+    ws[f'{idx[current_month]}{nii}'].value = f'={idx[current_month]}{gnii}-{idx[current_month]}{pcl}'
     ws[f'{idx[current_month]}{insurance}'].value = insurance_commissions
     ws[f'{idx[current_month]}{licensing_income}'].value = total_licensing_income
     ws[f'{idx[current_month]}{other_income}'].value = other_income_v
     ws[f'{idx[current_month]}{total_other}'].value = f'={idx[current_month]}{insurance}+{idx[current_month]}{licensing_income}+{idx[current_month]}{other_income}'
-    ws[f'{idx[current_month]}{nii_net}'].value = net_interest_income+total_other_income
+    ws[f'{idx[current_month]}{nii_net}'].value = f'={idx[current_month]}{nii}+{idx[current_month]}{total_other}'
     ws[f'{idx[current_month]}{sb}'].value = salaries_benefits
     ws[f'{idx[current_month]}{ooe}'].value = other_operating_expense
     ws[f'{idx[current_month]}{rc}'].value = misc_exp_rec
-    ws[f'{idx[current_month]}{toe}'].value = salaries_benefits+other_operating_expense+misc_exp_rec
-    ws[f'{idx[current_month]}{oibit}'].value = (net_income-total_operating_expenses)
+    ws[f'{idx[current_month]}{toe}'].value = f'={idx[current_month]}{sb}+{idx[current_month]}{ooe}+{idx[current_month]}{rc}'
+    ws[f'{idx[current_month]}{oibit}'].value = f'={idx[current_month]}{nii_net}-{idx[current_month]}{toe}'
     ws[f'{idx[current_month]}{pfit}'].value = income_tax_provision
-    ws[f'{idx[current_month]}{net_income_id}'].value = (net_income-total_operating_expenses)-income_tax_provision
-    wb.save(f'execute/{current_month} GAAP IS.xlsx')
+    ws[f'{idx[current_month]}{net_income_id}'].value = f'={idx[current_month]}{oibit}-{idx[current_month]}{pfit}'
+    return wb.save(f'execute/{current_month} GAAP IS.xlsx')
--- main.py
+++ main.py
@@ -1,33 +1,11 @@
+#Functionality code contained in '_functions.py'
+import os
+import re
+from pathlib import Path
+salaries_benefits_geo = 42
 current_month = '2021-12-31'
 import pandas as pd
-from _functions import *
 from openpyxl import Workbook, load_workbook
-wb = load_workbook('execute/GAAPIS.xlsx')
-ws = wb.active
-
-interest_income = '10'
-interest_expense = '11'
-gnii = '12'
-pcl_income = '13'
-nii = '14'
-insurance = '17'
-licensing_income = '18'
-other_ = '19'
-total_other = '20'
-nii_net = '24'
-sb = '27'
-ooe = '28'
-rc = '29'
-toe = '30'
-oibit = '32'
-pfit = '34'
-net_income = '36'
-
-dates = pd.Series(['2021-01-31', '2021-02-28', '2021-03-31','2021-04-30','2021-05-31','2021-06-30','2021-07-31','2021-08-31','2021-09-30','2021-10-31','2021-11-30','2021-12-31'])
-idx = pd.Series(['D','E','F','G','H','I','J','K','L','M','N','O'])
-idx.index = dates 
-print(idx) 
-
+from _functions import *
 
 income_statement()
-wb.save(f'execute/{current_month} GAAP IS.xlsx')
